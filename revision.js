88f45e07d982e852704f4b667024b925a46be2fa
evo264714 <mh264714@gmail.com>
2023-01-27 23:25:49 +0600

    first

---
+++ revision.js
@@ -0,0 +1,16 @@
+// * String type variable
+var bottle = 'water';
+var bottleColor = 'yellow';
+var penColor = 'blue';
+
+// * Number type Variable
+var sunglassPrice = 120;
+
+//* Boolean type variable
+var inExpensive = true;
+
+var tableItems = ['bottle', 'pen', 'mouse', 'paper', 'sunglass'];
+
+var item3 = tableItems[3];
+tableItems[4] = 'notebook';
+var penIndex = tableItems.indexOf('pen');
