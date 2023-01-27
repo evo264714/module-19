18a24ef16bdf557633ea91a7cf10011a876f63bd
evo264714 <mh264714@gmail.com>
2023-01-27 23:46:20 +0600

    while

--- while.js
+++ while.js
@@ -2,8 +2,6 @@ var roastGiven = 0;
 
 while(roastGiven < 7){
     console.log("roast given");
-    //* roastGiven = roastGiven + 1;
-    //* roastGiven += 1;
     console.log(roastGiven);
     roastGiven++;
 }
