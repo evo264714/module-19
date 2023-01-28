d9e328eaa184957d29c27683d0d5d51817475b2c
evo264714 <mh264714@gmail.com>
2023-01-28 15:57:24 +0600

    loop

---
+++ even.js
@@ -0,0 +1,6 @@
+//* Target: 1 to 10 all even numbers
+var numbers = 0;
+while(numbers <= 20){
+    console.log(numbers);
+    numbers = numbers + 2;
+}
--- while.js
+++ while.js
@@ -1,3 +1,9 @@
+/*
+* 1. Loop variable
+* 2. Condition inside while
+* 3. Loop body
+* 4. Change the loop variable
+*/
 var roastGiven = 0;
 
 while(roastGiven < 7){
