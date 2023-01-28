8199fc390ae0b9d635464b9f449f83c1fb5a5b7f
evo264714 <mh264714@gmail.com>
2023-01-28 17:13:33 +0600

    break

---
+++ for.js
@@ -0,0 +1,34 @@
+// var roastGiven = 0;
+// while(roastGiven < 7){
+    // console.log('Roast Given');
+//     roastGiven++;
+
+// }
+
+// for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+//     console.log('Roast Given')
+// }
+
+ //* Simple version of for loop
+console.log('Simple for loop');
+for(var i = 0; i < 7; i++){
+    console.log(i);
+}
+
+//* for loop declaration
+
+for(var i = 0; i < 10; i++){
+    console.log(i);
+}
+
+//* Even Numbers using for loop
+
+for(var i = 0; i <= 20; i += 2){
+    console.log(i);
+}
+
+//* Odd Numbers using for loop
+
+for(var i = 1; i <= 20; i += 2){
+    console.log(i);
+}
